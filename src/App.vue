ca217d293cfa0d94a127f8e891606caba8923d54
kevinkishorej <kishore2t81@gmail.com>
2021-03-09 21:00:36 +0530

    sta

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
+
 <template>
+ <v-app>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+   </v-app>
 </template>
 
-<script>
+
+<script> 
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -17,6 +21,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
